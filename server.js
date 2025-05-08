f598eb717384e8ff0eb138d5f1e16fdb37391fda
EMMY_HENZ-TECH <emmanueleffangahenshaw072@gmail.com>
2025-05-08 01:03:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs")
 const { v4: uuidv4 } = require("uuid")
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 7860
 
 // Middleware
 app.use(express.static("public"))
